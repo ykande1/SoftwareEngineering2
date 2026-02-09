3d153e5c9509eca9ef63838252c1d606a74fedd0
Typewriter <typewriter@Typewriter.local>
2026-02-09 14:53:13 -0500

    Added csv file, added new function names to work on - CEC.

--- url_shortener.py
+++ url_shortener.py
@@ -1,18 +1,40 @@
 #Charles Cain, 2.9.26
 #URL Shortener
 
-import validators
+import csv
+
+#this should be a relative path:
+url_file_path = "urls.csv"
 
 def shorten_url(url):
+    return 0
 
+def create_entry(url, short_url, id):
+    return [url, short_url, id]
 
-def create_json(url, short_url, id):
-    new_entry = {
+def write_csv(data, file_path):
+    with open(file_path) as
+    return -1
 
-    }
+def read_csv(file_path):
+    return -1
 
 def init():
     #start program, prompt user for urls
+    print("/-------------\\")
+    print("|URL Shortener|")
+    print("\-------------/")
+
+    while(True):
+        url_input = input("Enter a URL to shorten:\n")
+
+        #begin shortening process
+        current_file = open(url_file_path)
+
+        current_file.close()
+        shortened_url = shorten_url(url_input)
+
+        #create_json()
 
 if __name__ ==  "__main__":
     init()
