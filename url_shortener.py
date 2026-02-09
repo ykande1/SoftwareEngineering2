21f39805cb4d7495decc0eba952c9428fa3e01a0
Typewriter <typewriter@Typewriter.local>
2026-02-09 14:23:20 -0500

    First commit for url_shortener.py. Added method skeleton.

---
+++ url_shortener.py
@@ -0,0 +1,18 @@
+#Charles Cain, 2.9.26
+#URL Shortener
+
+import validators
+
+def shorten_url(url):
+
+
+def create_json(url, short_url, id):
+    new_entry = {
+
+    }
+
+def init():
+    #start program, prompt user for urls
+
+if __name__ ==  "__main__":
+    init()
